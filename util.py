0f0d93077a060f31b8b2b869b1ba5a5f7630c470
axl72 <axell.bernabel@unmsm.edu.pe>
2022-11-13 19:52:58 -0500

    Se restructuro el archivo

--- util.py
+++ util.py
@@ -1,29 +1,9 @@
-import datetime
-import calendar
+from procal import Calendar
 import random
 
 
-def get_current_calendar():
-    date = datetime.date.today()
-
-    return calendar.monthcalendar(date.year, date.month)
 
 
-def get_current_calendar_dict():
-    """Get a custom calendar content in Dict object"""
-
-    cal = get_current_calendar()
-
-    result = dict()
-    for index, day in enumerate(("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")):
-        result[day] = [day[index] for day in cal if day[index] != 0]
-
-    return result
-
-
-def is_hollyday(day):
-    # requiere implementation
-    return False
 
 
 def get_count_work_days(calendar_dictionary):
@@ -36,18 +16,7 @@ def get_count_work_days(calendar_dictionary):
     return len(result)
 
 
-def calculate_days_to_works(num_days):
-    junior = 28
-    num_days *= 3
-    if (num_days - 28) % 3 == 0:
-        senior = (num_days - 28)//3
-    elif (num_days - 28) % 3 == 2:
-        junior = 27
-        senior = (num_days - junior)//3
-    else:
-        senior = (num_days - 28)//3
 
-    return {"senior": senior, "junior": junior}
 
 
 def generate_workers_of_the_day(workers_dict, days_to_work):
@@ -64,8 +33,6 @@ def generate_workers_of_the_day(workers_dict, days_to_work):
     workers_dict[worker_of_the_day][0] += 1
     return worker_of_the_day
 
-def get_feriados() -> list:
-    return []
 
 def listas_diferentes(lista1:list, lista2:list) -> bool:
     for valor1 in lista1:
@@ -80,9 +47,26 @@ def listas_diferentes2(lista1: list, lista2: list) -> bool:
     return False
 
 
+
+def contar_cantidad_turnos_tipo(diccionario:dict):
+    result = dict()
+    for day, value in diccionario.items():
+        for turno in value:
+            tipo = value[turno]
+
+            for t in tipo:
+                if t in result:
+                    result[t] += 1
+                else:
+                    result[t] = 0
+    return result
+
 if __name__ == "__main__":
     hola = {"juana": 1, "sergio": 2}
 
-    [print(key, value) for key, value in hola.items()]
-    print(hola.values)
-    map(lambda x: print(x) if x == "juana" else None, hola.items())
+    # [print(key, value) for key, value in hola.items()]
+    # print(hola.values)
+    # map(lambda x: print(x) if x == "juana" else None, hola.items())
+    dias_del_mes = Calendar.get_days_list_of_month()
+    diccionario = {"mañana": {dia: {"cantidad":1, "tipo":"junior"} for dia in dias_del_mes if not Calendar.is_weekend_in_current_calendar(dia)}, "tarde": {dia: {"cantidad":2, "tipo":("senior", "junior")} for dia in dias_del_mes if not Calendar.is_weekend_in_current_calendar(dia)}}
+    print(contar_cantidad_turnos_tipo(diccionario))
