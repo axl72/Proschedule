4608160d89adf0f899181f573a487c33a28b140b
axl72 <axell.bernabel@unmsm.edu.pe>
2022-11-13 20:13:33 -0500

    Definición de funcinoes

--- templates.py
+++ templates.py
@@ -1,2 +1,7 @@
 class Template:
-    pass
+    def __init__(self, matrix):
+        self.content = Template.transformar_matrix_to_json(matrix)
+    
+    @staticmethod
+    def transformar_matrix_to_json(matrix):
+        pass
